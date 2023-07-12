063ffad1155e2ff554903e9820af65e6a285d2bf
Michael <128767960+michaelyang-21@users.noreply.github.com>
2023-07-12 14:03:24 -0400

    Tuning hyperparameters for delaney

--- solubility_test.py
+++ solubility_test.py
@@ -1,3 +1,5 @@
+# install libraries
+
 # !pip install rdkit
 # !pip install deepchem
 
@@ -13,6 +15,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 df = pd.read_csv("delaney-processed.csv")
+# df = pd.read_csv("curated-solubility-dataset.csv")
 
 rdkit_featurizer = dc.feat.RDKitDescriptors(use_fragment=False, ipc_avg=False)
 features = rdkit_featurizer(df.SMILES) # with one molecule
@@ -27,9 +30,9 @@ df0 = pd.DataFrame(data=features)
 df0.columns = column_names
 
 # adding molecule ids and solubility columns
+# 'ESOL' for delaney and 'Solubility' for Aqsoldb
 df0["SMILES"] = df.SMILES
 df0["ESOL"] = df.ESOL
-
 
 df0.columns
 df0.head()
@@ -38,18 +41,23 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report, confusion_matrix, mean_squared_error, mean_absolute_error
 import lightgbm as lgb
 from lightgbm import Dataset
-# from utils file
 
 y = df0.ESOL
 X = df0.drop(columns=["SMILES", "ESOL"])
 
+
+#Training and validation
+
+import math
 import lightgbm as lgb
 from sklearn.metrics import mean_squared_error, mean_absolute_error
 from sklearn.model_selection import train_test_split, KFold
 from sklearn.metrics import r2_score
+from sklearn.metrics import classification_report, confusion_matrix, mean_squared_error, mean_absolute_error, mean_absolute_percentage_error
+from scipy import stats
 
 # Split the data into training and combined validation/testing set
-X_train_val, X_test, y_train_val, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+X_train_val, X_test, y_train_val, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # Split the combined validation/testing set into validation and testing set
 X_val, X_test, y_val, y_test = train_test_split(X_test, y_test, test_size=0.5, random_state=42)
@@ -57,11 +65,11 @@ X_val, X_test, y_val, y_test = train_test_split(X_test, y_test, test_size=0.5, r
 # Define the LightGBM parameters
 params = {
     'objective': 'regression',
-    'metric': 'mse',
+    'metric': 'rmse',
     'boosting': 'gbdt',
     'num_leaves': 40,
-    'learning_rate': 0.05,
-    'feature_fraction': 0.68,
+    'learning_rate': 0.1,
+    'feature_fraction': 0.9,
     'bagging_fraction': 0.8,
     'bagging_freq': 5,
     'verbose': 0
@@ -83,7 +91,7 @@ for train_index, val_index in kf.split(X_train_val):
     train_data = lgb.Dataset(X_train, label=y_train)
     val_data = lgb.Dataset(X_val, label=y_val)
 
-    model = lgb.train(params, train_data, num_boost_round=100, valid_sets=[train_data, val_data], early_stopping_rounds=10)
+    model = lgb.train(params, train_data, num_boost_round=1000, valid_sets=[train_data, val_data], early_stopping_rounds=10)
 
     y_val_pred = model.predict(X_val)
     mse = mean_squared_error(y_val, y_val_pred)
@@ -99,7 +107,7 @@ print("Average Mean Squared Error:", avg_mse)
 print("Average Mean Absolute Error:", avg_mae)
 
 # Train final model on the entire training dataset
-final_model = lgb.train(params, data, num_boost_round=100)
+final_model = lgb.train(params, data, num_boost_round=1000)
 
 # Make predictions on the testing set
 y_test_pred = final_model.predict(X_test)
@@ -109,24 +117,18 @@ mse_test = mean_squared_error(y_test, y_test_pred)
 mae_test = mean_absolute_error(y_test, y_test_pred)
 r = r2_score(y_test, y_test_pred)
 
-print("Final Model - Mean Squared Error on Testing Set:", mse_test)
-print("Final Model - Mean Absolute Error on Testing Set:", mae_test)
-print("Final Model - R^2 on Testing Set:", r)
+performance = pd.DataFrame([[mse_test], [mae_test], [r], [math.sqrt(mse_test)]],
+                   columns=['Final Model Test Set'], index = ['Mean Square Error', 'Mean Absolute Error', 'R^2', 'RMSE'])
+performance
 
+# Spearman correlation
+stats.spearmanr(y_test, y_test_pred)
+
+# Pearson correlation
+stats.pearsonr(y_test, y_test_pred)
 
 tmp = pd.DataFrame(data={"y_true":  y_test, "y_proba": y_test_pred})
 sns.scatterplot(x="y_true", y="y_proba", data=tmp);
 plt.title("Real vs Predicted Solubility")
 plt.xlabel("Real Solubility")
 plt.ylabel("Predicted Solubility");
-
-
-
-
-
-
-
-
-
-
-
